84e9b2a903f8a8f3c265ff690b21749305754119
Allen Li <20001130li@gmail.com>
2020-08-12 05:52:37 -0400

    add check file exits

--- products.py
+++ products.py
@@ -9,15 +9,33 @@
 "*清单中还有清单/2 dimension"
 "*split/切割, split()括号里面是你想要分开的那一个东西, split切完之后就是list"
 "*continue/继续: 跳到下一回"
-#读取档案
+"*os/作业系统: operating system"
+
+#检查档案
 products = []  
-with open('products.csv', 'r') as f:
+import os
+if os.path.isfile('products.csv'):
+	print('yeah!')
+	with open('products.csv', 'r') as f:
+		for line in f:
+			if '商品, 价格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])       
+	print(products)
+
+else:
+	print('No!')
+
+#读取档案
+#products = []  
+#with open('products.csv', 'r') as f:
 	for line in f:
 		if '商品, 价格' in line:
 			continue
 		name, price = line.strip().split(',')
 		products.append([name, price])       
-print(products)
+#print(products)
 
 while True:
 	name = input('请输入商品名称: ')                     #大清单
@@ -33,14 +51,14 @@ print(products)
 
 #products[0][0]                                               #大清单里面第0格小清单的第0格                                     
 
-#印出所以商品记录                                    
+#印出所有商品记录                                    
 for p in products:                                        #让大清单中的每一个小清单/p一个一个的list出来
 	print(p[0], '的价格是', p[1])                          #把小清单里面的第一个一个一个的列出来
                                    
 #'abc' + 123 = 'abc123'
 #'abc' * 3 ='abcabcabc'
 
-#记录商品
+#写入档案
 with open('products.csv', 'w') as f:                      #csv是来存储网站的能用excel/表格打开(,是区隔)
 	f.write('商品, 价格\n')
 	for p in  products:
